4bf65fb2bb40a4995ca979106f479b72552d3220
sauravuprety21 <saurav.uprety1@ucalgary.ca>
2025-10-19 14:29:46 -0600

    Add syncout time publisher

--- src/xsens_mti_ros2_driver/src/messagepublishers/statuspublisher.h
+++ src/xsens_mti_ros2_driver/src/messagepublishers/statuspublisher.h
@@ -1,34 +1,33 @@
 //  Copyright (c) 2003-2024 Movella Technologies B.V. or subsidiaries worldwide.
 //  All rights reserved.
-//  
+//
 //  Redistribution and use in source and binary forms, with or without modification,
 //  are permitted provided that the following conditions are met:
-//  
+//
 //  1.	Redistributions of source code must retain the above copyright notice,
 //  	this list of conditions, and the following disclaimer.
-//  
+//
 //  2.	Redistributions in binary form must reproduce the above copyright notice,
 //  	this list of conditions, and the following disclaimer in the documentation
 //  	and/or other materials provided with the distribution.
-//  
+//
 //  3.	Neither the names of the copyright holders nor the names of their contributors
 //  	may be used to endorse or promote products derived from this software without
 //  	specific prior written permission.
-//  
+//
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 //  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 //  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
 //  THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-//  SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+//  SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
 //  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
 //  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY OR
 //  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.THE LAWS OF THE NETHERLANDS 
-//  SHALL BE EXCLUSIVELY APPLICABLE AND ANY DISPUTES SHALL BE FINALLY SETTLED UNDER THE RULES 
-//  OF ARBITRATION OF THE INTERNATIONAL CHAMBER OF COMMERCE IN THE HAGUE BY ONE OR MORE 
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.THE LAWS OF THE NETHERLANDS
+//  SHALL BE EXCLUSIVELY APPLICABLE AND ANY DISPUTES SHALL BE FINALLY SETTLED UNDER THE RULES
+//  OF ARBITRATION OF THE INTERNATIONAL CHAMBER OF COMMERCE IN THE HAGUE BY ONE OR MORE
 //  ARBITRATORS APPOINTED IN ACCORDANCE WITH SAID RULES.
-//  
-
+//
 
 #ifndef STATUSPUBLISHER_H
 #define STATUSPUBLISHER_H
@@ -36,20 +35,22 @@
 #include "packetcallback.h"
 #include "xsens_mti_ros2_driver/msg/xs_status_word.hpp"
 
-
 struct StatusPublisher : public PacketCallback
 {
     rclcpp::Publisher<xsens_mti_ros2_driver::msg::XsStatusWord>::SharedPtr pub;
-    //std::string frame_id = DEFAULT_FRAME_ID;
+    rclcpp::Publisher<sensor_msgs::msg::TimeReference>::SharedPtr pub_syncout;
+
+    // std::string frame_id = DEFAULT_FRAME_ID;
 
     StatusPublisher(rclcpp::Node::SharedPtr node)
     {
         int pub_queue_size = 5;
 
         node->get_parameter("publisher_queue_size", pub_queue_size);
-        //node->get_parameter("frame_id", frame_id);
+        // node->get_parameter("frame_id", frame_id);
 
         pub = node->create_publisher<xsens_mti_ros2_driver::msg::XsStatusWord>("~/status", pub_queue_size);
+        pub_syncout = node->create_publisher<sensor_msgs::msg::TimeReference>("~/sync_out", pub_queue_size);
     }
 
     void parseToMessage(xsens_mti_ros2_driver::msg::XsStatusWord &msg, uint32_t status)
@@ -117,16 +118,28 @@ struct StatusPublisher : public PacketCallback
         msg.rtk_status = (status >> 27) & 0x3; // status & 0x18000000;
     }
 
+    bool statusContainsMarker(uint32_t status, uint32_t pos)
+    {
+        return ((status & (1 << pos)) != 0);
+    }
+
     void operator()(const XsDataPacket &packet, rclcpp::Time timestamp)
     {
         if (packet.containsStatus())
         {
-            xsens_mti_ros2_driver::msg::XsStatusWord msg;
+            xsens_mti_ros2_driver::msg::XsStatusWord msgStatus;
 
             uint32_t status = packet.status();
-            parseToMessage(msg, status);
+            parseToMessage(msgStatus, status);
 
-            pub->publish(msg);
+            static uint32_t posSyncOut = 22;
+            if (statusContainsMarker(status, posSyncOut))
+            {
+                sensor_msgs::msg::TimeReference msgTimeRef;
+                msgTimeRef.header.stamp = timestamp;
+                pub_syncout->publish(msgTimeRef);
+            }
+            pub->publish(msgStatus);
         }
     }
 };
